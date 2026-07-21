8fc46581be4ddcf8562352efebf5c8d85d3d115d
AswinSatheesh <aswinsatheesh2001@gmail.com>
2026-07-21 21:34:05 +0530

    login screen

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -10,7 +10,7 @@ interface AuthContextType{
 const AuthContext = createContext<AuthContextType | undefined>(undefined);
 
 export const AuthProvider =({children} : {children : ReactNode})=>{ //ReactNode : "extract children from the props object, and ensure that children is of type ReactNode (meaning it can be text, HTML tags, or other React components).
-    const[isAuthenticated,setIsAuthenticated] = useState<boolean>(true); //true means user authenticated - false means allow all routing
+    const[isAuthenticated,setIsAuthenticated] = useState<boolean>(false); //true means user authenticated - false means allow all routing
     const login = ()=> setIsAuthenticated(true);
     const logout = ()=> setIsAuthenticated(false);
 
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,103 @@
+import { useNavigate } from "react-router";
+import { useAuth } from "../context/AuthContext";
+import { useState } from "react";
+
 export const Login =() =>{
+    // 1. Listen to our global Context tower to grab the login action
+    const {login} = useAuth();
+    // 2. Grab React Router's navigation tool to redirect pages
+    const navigate = useNavigate();
+
+    const[email,setEmail] =useState('');
+    const[password,setPassword] = useState('');
+
+    const handleSubmit =(e : React.SubmitEvent)=>{
+        e.preventDefault(); //stop the page reload.
+
+        if(!email || !password){
+            alert("Please enter both email and password");
+            return;
+        }
+        // 1. Flip global state: sets isAuthenticated to true
+        login();
+
+        // 2. Redirect: sends the user to the protected Dashboard route
+        navigate('/');
+    }
     return(
-        <div style={{padding : '2rem'}}>
-            <h1>Login Screen</h1>
-            <p>Welcome back to DocMind. Secure sign-in goes here.</p>
+        <div style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            minHeight: '80vh',
+        }}>
+            <form onSubmit={handleSubmit}
+                style={{
+                    background: 'white',
+                    padding: '2.5rem',
+                    borderRadius: '8px',
+                    boxShadow: '0 4px 6px -1px rgba(0, 0, 0, 0.1)',
+                    width: '100%',
+                    maxWidth: '400px',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    gap: '10px'
+                }}
+                >
+                    <h2 style={{margin:'0',textAlign: 'center',color: '#1e293b'}}>Welcome Back</h2>
+                    <p style={{ fontSize: '0.875rem', fontWeight: '600', color: '#334155' }}>Login in to access your DocMind dashboard</p>
+                {/* Email Input bound to email state */}
+                <div 
+                    style={{display: 'flex',flexDirection: 'column',gap: '0.5rem'}}>
+                    <label style={{ fontSize: '0.875rem', fontWeight: '600', color: '#334155' }}>Email Address</label>
+                    <input 
+                        type="email"
+                        value={email}
+                        onChange={(e)=> setEmail(e.target.value)}
+                        placeholder="user@example.com"
+                        style={{
+                            padding: '0.625rem',
+                            borderRadius: '6px',
+                            border: '1px solid #cbd5e1',
+                            outline: 'none'
+                        }}
+                    />
+                </div>
+
+                {/* Password Input bound to password state */}
+                <div style={{
+                    display :'flex',
+                    flexDirection: 'column',
+                    gap: '0.5rem'
+                }}>
+                    <label style={{ fontSize: '0.875rem', fontWeight: '600', color: '#334155' }}> Password </label>
+                    <input 
+                        type="password"
+                        value={password}
+                        onChange={(e)=> setPassword(e.target.value)}
+                        placeholder="••••••••"
+                        style={{ 
+                            padding: '0.625rem', 
+                            borderRadius: '6px', 
+                            border: '1px solid #cbd5e1',
+                            outline: 'none'
+                            }}
+                    />
+                </div>
+
+                <button type="submit"
+                    style={{
+                        background: '#2563eb',
+                        color: "white",
+                        border: 'none',
+                        padding: '0.75rem',
+                        borderRadius: '6px',
+                        fontWeight: 'bold',
+                        cursor: 'pointer',
+                        marginTop: '0.5rem'
+                    }}
+                >Sign In</button>
+            </form>
         </div>
     );
 }
